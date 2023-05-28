efdc2b2c9a134ed6da37cc7bc2966712713d6d41
thekamalkashyap <iamkamalkumar@proton.me>
2023-05-28 13:53:59 +0530

    content updated

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Lottie from "lottie-react";
-import camera from "../../public/camera.json";
+import camera from "../../public/cameradark.json";
 
 const Hero = () => {
   const cameraRef = React.useRef();
   return (
-    <div className="hero min-h-screen bg-black">
+    <div className="hero bg-[url('/bg.jpeg')] min-h-screen bg-black">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <Lottie
+        {/* <Lottie
           lottieRef={cameraRef}
           onComplete={() => {
             cameraRef.current.goToAndPlay(120, true);
@@ -15,15 +15,16 @@ const Hero = () => {
           className="lottie-hero"
           animationData={camera}
           loop={false}
-        />
-        <div className=" w-[300px] md:w-[450px] ">
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+        /> */}
+        {/* <div className=" w-[300px] md:w-[450px] ">
+          <h1 className="text-5xl font-bold">Wishvaas khoraa </h1>
+          <h2 className="text-3xl opacity-80 font-bold">Director - editor</h2>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -30,19 +30,22 @@ export default function Home() {
 
   const data = [
     {
-      heading: "Out There",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error fuga, dolorum",
+      heading: "About me",
+      description: `I have always had a staunch interest in films, music
+      videos and visual media. Throughout my schooling, I have always made 
+       it a point to engage in extracurricular activities related to this 
+       field and having a lot of passion for filmmaking, brought me an 
+       opportunity to get engaged in an internship, as an Editor and Assistant
+        Director, during my high school. Till the end of my schooling, I have
+         been part of more than 30 music videos as Chief Assistant Director,
+          Editor and VFX Artists in numerous independent projects.`,
     },
     {
       heading: "Immersion",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error fuga, dolorum",
-    },
-    {
-      heading: "To Infinity",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error fuga, dolorum",
+      description: `I believe that, with multitasking a wish to turn into a 
+      Superman can be made possible. Here I introduce myself well inclined to 
+      Direction, Video Editing, Graphic designing, Writing, Sketching and music
+       programming.`,
     },
   ];
 
@@ -59,18 +62,18 @@ export default function Home() {
                 isEven
                   ? "bg-[#1a1a1a] md:flex-row text-white"
                   : "md:flex-row-reverse bg-white text-black "
-              } px-24 w-screen h-screen flex flex-col-reverse justify-center items-center `}
+              } px-2 md:px-24 w-screen h-screen flex flex-col-reverse justify-center items-center `}
             >
               <div
                 className={`flex-1 flex flex-col justify-center items-center ${
                   isEven ? "md:items-start" : "md:items-end text-right"
                 } `}
               >
-                <h2>{info.heading}</h2>
-                <div className="max-w-[25rem]">{info.description}</div>
+                <h2 className=" text-4xl font-bold my-6 ">{info.heading}</h2>
+                <p className="max-w-[35rem] text-xs md:text-base">{info.description}</p>
               </div>
-              <div className="flex-1 flex justify-center items-center">
-                <img src="./1.png" alt={`img-${index + 1}`} />
+              <div className="flex-1 mx-6 flex justify-center items-center">
+                <img className=" rounded-xl " src="./bg.jpeg" alt={`img-${index + 1}`} />
               </div>
             </section>
           );
